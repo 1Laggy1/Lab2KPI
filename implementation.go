646816c0c6414e06721ef5e82e76c20853e9c62b
Moriarty <seylvin2005@gmail.com>
2024-03-21 15:19:55 +0200

    Added code to implementation -Added implementation of converting  Postfix to Infix form

--- implementation.go
+++ implementation.go
@@ -1,9 +1,49 @@
 package lab2
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-// TODO: document this function.
-// PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+func postfixToInfix(expression string) (string, error) {
+	stack := []string{}
+
+	tokens := strings.Fields(expression)
+
+	for _, token := range tokens {
+		if isOperator(token) {
+			if len(stack) < 2 {
+				return "", fmt.Errorf("invalid expression: insufficient operands")
+			}
+
+			operand2 := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			operand1 := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+			stack = append(stack, "("+operand1+token+operand2+")")
+		} else {
+			stack = append(stack, token)
+		}
+	}
+
+	if len(stack) != 1 {
+		return "", fmt.Errorf("invalid expression: invalid number of operands")
+	}
+
+	return stack[0], nil
+}
+
+func isOperator(token string) bool {
+	return token == "+" || token == "-" || token == "*" || token == "/" || token == "^"
+}
+
+func main() {
+	postfixExpression := "10 2 8 * 4 / + 3 -"
+	infixExpression, err := postfixToInfix(postfixExpression)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Println("Infix expression:", infixExpression)
 }
